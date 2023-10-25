9203516c85bf23e33251fa0609f925afc455c31e
skrishnan22 <skrish22195@gmail.com>
2023-10-25 23:07:19 +0530

    Add prompt as a cli option

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import axios from 'axios';
 const OPEN_AI_URL = 'https://api.openai.com/v1/images/generations';
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
@@ -5,7 +6,12 @@ import ora from 'ora';
 import chalk from 'chalk';
 
 import terminalImage from 'terminal-image';
-const promptText = `A realistic and natural photo of a girl on a hill or mountain top watching the sunrise with her friend. In studio gibli style`;
+// const promptText = `A realistic and natural photo of a girl on a hill or mountain top watching the sunrise with her friend. In studio gibli style`;
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+const args = yargs(hideBin(process.argv)).parse()
+
+yargs(hideBin(process.argv))
 
 async function sendRequestToOpenAI({ promptText }) {
   return axios.post(
@@ -23,8 +29,12 @@ async function sendRequestToOpenAI({ promptText }) {
   );
 }
 async function run() {
+  if(!args.p){
+    throw new Error('Please provide a prompt');
+  }
+
   const spinner = ora({text:`${chalk.blue('Generating image(s)...')}`, spinner: 'runner'}).start();
-  const img = await sendRequestToOpenAI({ promptText });
+  const img = await sendRequestToOpenAI({ promptText: args.p });
   spinner.succeed('Your image is now ready');
   const imgUrl = img.data.data;
   for (const url of imgUrl) {
