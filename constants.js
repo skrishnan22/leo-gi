055b92367396a15bb0a1b64ac434384ce1a060ab
skrishnan22 <skrish22195@gmail.com>
2023-10-28 13:36:40 +0530

    Add option for image size

---
+++ constants.js
@@ -0,0 +1,7 @@
+export const OPEN_AI_URL = 'https://api.openai.com/v1/images/generations';
+
+export const IMAGE_SIZE_MAP = {
+    's': '256x256',
+    'm': '512x512',
+    'l': '1024x1024'
+}
--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 #! /usr/bin/env node
 import axios from 'axios';
-const OPEN_AI_URL = 'https://api.openai.com/v1/images/generations';
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+import * as CONSTANTS from './constants.js';
 import ora from 'ora';
 import chalk from 'chalk';
-
+import { writeFile }  from 'fs/promises'
 import terminalImage from 'terminal-image';
 // const promptText = `A realistic and natural photo of a girl on a hill or mountain top watching the sunrise with her friend. In studio gibli style`;
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
-const args = yargs(hideBin(process.argv)).parse()
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+const args = yargs(hideBin(process.argv)).parse();
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
-yargs(hideBin(process.argv))
+yargs(hideBin(process.argv));
 
-async function sendRequestToOpenAI({ promptText }) {
+async function sendRequestToOpenAI({ promptText, imageSize = 'm' }) {
   return axios.post(
-    OPEN_AI_URL,
+    CONSTANTS.OPEN_AI_URL,
     {
       prompt: promptText,
-      n: 1
+      n: 1,
+      size: CONSTANTS.IMAGE_SIZE_MAP[imageSize]
     },
     {
       headers: {
@@ -28,18 +29,24 @@ async function sendRequestToOpenAI({ promptText }) {
     }
   );
 }
-async function run() {
-  if(!args.p){
+function validateArgs() {
+  if (!args.p) {
     throw new Error('Please provide a prompt');
   }
-
-  const spinner = ora({text:`${chalk.blue('Generating image(s)...')}`, spinner: 'runner'}).start();
-  const img = await sendRequestToOpenAI({ promptText: args.p });
+  if (args.s && !CONSTANTS.IMAGE_SIZE_MAP[args.s.toLowerCase()]) {
+    throw new Error('Invalid option for size. Please use one of s,m,l');
+  }
+}
+async function run() {
+  validateArgs();
+  const spinner = ora({ text: `${chalk.blue('Generating image(s)...')}`, spinner: 'runner' }).start();
+  const img = await sendRequestToOpenAI({ promptText: args.p, imageSize: args?.s?.toLowerCase() });
   spinner.succeed('Your image is now ready');
   const imgUrl = img.data.data;
   for (const url of imgUrl) {
     const response = await axios.get(url.url, { responseType: 'arraybuffer' });
-    console.log(await terminalImage.buffer(response.data, { width: '50%', height: '50%' }));
+    console.log(await terminalImage.buffer(response.data, {width: '50%', height: '50%'}));
+    await writeFile(`${args.p}.png`, response.data)
   }
 }
 
